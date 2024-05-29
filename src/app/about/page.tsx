5ae3e13d1c56f44a2eaa5abc97cda8253bbe9d92
Saputra <saputra.uta50@gmail.com>
2024-05-29 22:37:31 +0800

    Link component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export const metadata = {
   title: 'About STRA Company'
 }
 
 export default function About() {
   return (
-    <h1>About</h1>
+    <>
+      <h1>About</h1>
+      <Link href="/">Home</Link>
+    </>
   )
 }
