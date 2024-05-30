7fac19d67e74716d2c98cf8d5ff91593d0d85266
Saputra <saputra.uta50@gmail.com>
2024-05-30 13:36:11 +0800

    button to try again

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-export default function ErrorBoundary({error} : {error: Error}) {
+export default function ErrorBoundary({error, reset} : {error: Error, reset: () => void}) {
   return (
-    <div>{error.message}</div>
+    <div>{error.message} <button onClick={reset}>Try again</button></div>
   )
 }
