b6c6031dd41b3731aeab77efe4513cc378bf182b
Saputra <saputra.uta50@gmail.com>
2024-05-30 13:28:51 +0800

    error handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,13 @@
 import { notFound } from "next/navigation";
+
+function getRandomInt(n: number) {
+  return Math.floor(Math.random() * n);
+}
 export default function ReviewDetail({ params }: { params: { productId: string, reviewId: string } }) {
+  const random = getRandomInt(2);
+  if (random === 1){
+    throw new Error("Error loading review");
+  }
   if (parseInt(params.reviewId) > 10) {
     notFound();
   }
